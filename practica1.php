2aab04def0cad386cf70681f95a7d85a7a87b4c3
JoseLuisZN3 <124735572+JoseLuisZN3@users.noreply.github.com>
2023-05-24 18:42:46 -0700

    Add files via upload
    parcial_3

---
+++ practica1.php
@@ -0,0 +1,19 @@
+<?php
+  echo "<h1> Hola Mundo</h1>";
+
+  echo "<hr>";
+
+  $nombre = "luis";
+  $edad = 16;
+
+  if($edad >16) {
+    echo $nombre . " es mayor de edad";
+  }else{
+    echo $nombre . " es menor de edad";
+  }
+  echo "<br><br>";
+
+  for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+  }
+?> 
